588f5128d4bb7001350e1cb3fa2bcb7d4e526b8b
David Riewe <david.riewe@gmail.com>
2017-01-04 14:14:57 -0600

    update

---
+++ websites/frenchscene.net/Gui/pages/live-music-in-france.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+
+<title>Live Music in France</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="copyright" content="FrenchScene.Net">
+<meta name="author" content="FrenchScene.Net">
+<meta name="email" content="frenchfever@frenchscene.net">
+<meta name="Distribution" content="Global">
+<meta name="Rating" content="General">
+<meta name="Robots" content="INDEX,FOLLOW">
+<meta name="Revisit-after" content="1 Day">
+
+<link rel="stylesheet" type="text/css" href="http://www.frenchscene.net/Gui/css/main.css" />
+
+<link rel="stylesheet" type="text/css" href="http://www.frenchscene.net/Gui/css/form.css" />
+
+<link rel="stylesheet" type="text/css" href="http://www.frenchscene.net/Gui/css/form.import.css" />
+
+
+</head>
+
+<body>
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-3812368-2");
+pageTracker._trackPageview();
+</script>
+
+<div id="container">
+
+<!-- begin container !-->
+
+<div id="menu">
+
+<a href="http://www.frenchscene.net/index.php">Home</a> | 
+<a href="http://www.frenchscene.net/french-song-search">French Songs</a>
+| Articles | 
+Great Moments | 
+Community
+
+</div>
+
+
+<div id="topbanner">
+<img src="http://www.frenchscene.net/Gui/images/site/topbanner.jpg" width="980" height="350" border="0" usemap="#Map" />
+</div>
+
+<div align="center" id="banner">
+<a href="http://50f6cd13h2n5z4wcplqgi73k24.hop.clickbank.net" onClick="javascript: pageTracker._trackPageview
+('/bannerads/rocketfrench/learnfrench');"> <img src="http://www.frenchscene.net/Gui/images/site/learnfrench.jpg"  /> </a>
+</div>
+<br />
+
+<div id="contentcontainer"> 
+
+ <!-- begin-content-container !--><div id="right">
+
+
+<div class="smallbox">
+<!-- begin song -->
+<h1>Featured French Song</h1>
+
+
+<div class="right"><img  height="100px" src="http://www.frenchscene.net/Gui/images/artists/laurent_konrad-discobitch_small.jpg" /></div><div class="left"><p>Artist:<a href="/artists/17/biography-discobitch">Discobitch </a><br />Song:<a href="/french-songs/49/clips-c-est-beau-la-bourgeoisie-by-discobitch">C'est Beau la Bourgeoisie</a><br />
+Released: 2008 <br /> <br /><a href="/french-songs/49/clips-c-est-beau-la-bourgeoisie-by-discobitch">Click here to listen now!</a></p></div>
+
+<!--end song -->
+</div>
+
+
+<div class="smallbox">
+<!-- begin song -->
+<h1>Featured French Classic</h1>
+
+
+<div class="right"><img  height="100px" src="http://www.frenchscene.net/Gui/images/artists/axellered152_small.jpg" /></div><div class="left"><p>Artist:<a href="/artists/3/biography-axelle-red">Axelle Red</a><br />Song:<a href="/french-songs/38/clips-sensualite-by-axelle-red">Sensualité </a><br />
+Released: 1993 <br /> <br /><a href="/french-songs/38/clips-sensualite-by-axelle-red">Click here to listen now!</a></p></div>
+
+<!--end song -->
+</div>
+
+
+</div>
+
+<div id="center">
+
+<div class="centerContent">
+  <h1>Live Music in France</h1> <br />
+  <br />
+  If you like listening to French music, then you'e sure to love listening to it played live - and where better to do so than in France itself? If you're unfamiliar with the live music scene in France, then here are just a few of the venues to which you might consider paying a visit. <br />
+  <br />
+  Wayne's <br />
+  <br />
+  Wayne's bar is located in Nice, and boasts live music every single night. Although the evenings showing international sports matches can get busy, head down on a Thursday for a chilled out atmosphere, and love acoustic music. If you're looking to dance the night away to classic rock and pop tunes, then this is the place for you. <br />
+  <br />
+  Le Carmen <br />
+  <br />
+  If you're looking for something a little more upmarket, then Le Carmen us certainly well worth a visit. If you've spent the day sat in an office chair, looking at spreadsheets or playing <a href="http://www.cheekybingo.com/" target="_blank">CheekyBingo</a>, then this live music venue can offer an indulgent and luxurious alternative come the evening. You'll hear music from some of the trendiest musicians in Paris and around the rest of the world - so what are you waiting for? <br />
+  <br />
+  Thelonius <br />
+  <br />
+  If you're keen to catch up-and-coming talent, then head to Bordeaux's Thelonius bar. Perfect for jazz fans, this atmospheric venue hosts performances from some of the hottest solo musicians in town. If you want to be the first to see the next big thing in the world of jazz, then this is the place to be. <br />
+  <br />
+  Autour de Midi <br />
+  <br />
+Hidden behind the better known Moulin Rouge venue, the Autour de Midi is a cellar-style venue, featuring everything from jazz, to swing, bebop tunes. If you're a looking for a laid-back alternative to many of Paris' more high-profile jazz clubs, then this is certainly something of a hidden treasure. </div>
+
+
+
+ <!--end content-container !-->
+</div>
+ 
+ <!--end container !-->
+</div>
+ 
+  
+</body>
+<map name="Map" id="Map"><area shape="rect" coords="252,212,747,259" href="http://www.frenchscene.net/FrenchScene/community/viewtopic.php?f=4&amp;t=3" target="_self" />
+</html>
